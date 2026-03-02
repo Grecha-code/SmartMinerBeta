9e0036f8337061f677113425aa0318a8f966ea47
Grecha-code <alex.pashkov2012@gmail.com>
2026-03-02 12:26:55 +0700

    Add files via upload

---
+++ code/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect, url_for
+from werkzeug.exceptions import HTTPException
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def reg():
+    try:
+        with open('example.txt', 'r') as file:
+            content = file.read()
+            return redirect(url_for('safety'))
+    except FileNotFoundError:
+        return render_template('registration.html')
+
+
+@app.route('/safety')
+def safety():
+    return render_template('safety.html')
+
+
+@app.errorhandler(HTTPException)
+def handle_error(error):
+    return render_template('error.html'), error.code
+
+
+if  __name__ == "__main__":
+    app.run(debug=True)
